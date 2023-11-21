3e35b4230006c91a1e08a346d85c0878458363a2
Dello96 <enadios03@gmail.com>
2023-11-22 01:28:17 +0900

    메인이미지 및 초반세팅

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import mainImg from "../assets/breadMain.png";
+
+const HomeImg = styled.img`
+  display: flex;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const ContentsInput = styled.input`
+  display: flex;
+  margin-left: 100px;
+  width: 300px;
+  height: 100px;
+  justify-content: center;
+  text-align: start;
+`;
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <nav>
+        <HomeImg src={mainImg}></HomeImg>
+      </nav>
+      <div>
+        <button>파일첨부</button>
+        <ContentsInput
+          type="text"
+          placeholder="오늘의 빵을 공유해주세요!"
+        ></ContentsInput>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
