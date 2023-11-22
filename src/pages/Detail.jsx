87a3a38c8f4d67ff3192e46d771a13b40ca6d928
areumlee1117 <aaareum1117@gmail.com>
2023-11-22 22:16:08 +0900

    사진 창 전환 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,96 +3,99 @@ import styled from "styled-components";
 import defaultUser from "../assets/no-image.gif";
 import Photos from "../components/Photos";
 
-
 function Detail() {
-    return <Container>
-<ButtonHome>Home</ButtonHome>
-<Header>
-    <ProfileImg>
-    <img src={null ?? defaultUser} alt="프로필이미지" />
-    </ProfileImg>
-    <Title>게시물 제목</Title>
-    <Date>2023/11/22</Date>
-</Header>
+  return (
+    <Container>
+      <ButtonHome>Home</ButtonHome>
+      <Header>
+        <ProfileImg>
+          <img src={null ?? defaultUser} alt="프로필이미지" />
+        </ProfileImg>
+        <Title>게시물 제목</Title>
+        <Date>2023/11/22</Date>
+      </Header>
 
-<Post>
-    <Photos />
-    <Review>작성자가 쓴 리뷰 넣을 거임 텍스트</Review>
-</Post>
-<Button>Top</Button>
-</Container>
+      <Post>
+        <Photos />
+        <Review>작성자가 쓴 텍스트 넣을 거임</Review>
+      </Post>
+      <Button>Top</Button>
+    </Container>
+  );
 }
 
 export default Detail;
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Header = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    width: 1200px;
-    height: 70px;
-    background-color: lightgray;
-    text-align: center;
-    padding: 24px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 1200px;
+  height: 70px;
+  background-color: lightgray;
+  text-align: center;
+  padding: 24px;
+`;
 
 const Title = styled.h1`
-    font-size: 30px;
-    font-weight:600;
-    color: brown;
-    align-items: center;
-    padding: 24px;
+  font-size: 30px;
+  font-weight: 600;
+  color: brown;
+  align-items: center;
 `;
 
 const Date = styled.p`
-    font-size: 15px;
-    color: blue;
-    margin-left: 100px;
-`
+  font-size: 15px;
+  color: blue;
+  margin-left: 500px;
+`;
 const Post = styled.li`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border: 2px solid black;
-    border-radius: 20px;
-    height: 500px;
-    width: 1200px;
-    margin: 20px
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid black;
+  border-radius: 20px;
+  height: 500px;
+  width: 1200px;
+  margin: 20px;
 `;
 
 const Review = styled.main`
-    flex-direction: row;
-    color: black;
-    background-color: yellow; 
+  flex-direction: row;
+  color: black;
+  background-color: yellow;
 `;
 
 const Button = styled.button`
-    position: fixed;
-    right: 40px;
-    bottom: 40px;
-`
+  cursor: pointer;
+  position: fixed;
+  right: 40px;
+  bottom: 40px;
+`;
+
 const ButtonHome = styled.button`
-    position: fixed;
-    left: 40px;
-    top: 40px;
-`
+  cursor: pointer;
+  position: fixed;
+  left: 40px;
+  top: 40px;
+`;
 
 const ProfileImg = styled.figure`
-    width: 50px;
-    height: 50px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
     border-radius: 50%;
-    overflow: hidden;
-    & img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        border-radius: 50%;
-    }
+  }
 `;
