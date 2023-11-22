f3d1092ce663b3110edec6695ba4c1dd3433792a
hanjiwoo <hanmingyu3@naver.com>
2023-11-22 13:43:46 +0900

    한지우초안

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyles from "./shared/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <>
-        <App />
-        <GlobalStyles />
-    </>
+  <>
+    <App />
+    <GlobalStyles />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -5,16 +5,16 @@ import Detail from "../pages/Detail";
 import MyPage from "../pages/MyPage";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/detail" element={<Detail />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/write" element={<Write />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
