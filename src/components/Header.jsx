0c6d4dcf2606d5c96e92d71007396397d48362b9
Dello96 <enadios03@gmail.com>
2023-11-23 17:31:00 +0900

    Home Navigation연결완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: large;
-  border-radius: 5px;
-`;
-
 export default function Header() {
   return (
     <HeadWrapper>
