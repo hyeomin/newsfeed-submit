29125cbb712672f8d87ed15cd6a65723c928635f
Dello96 <enadios03@gmail.com>
2023-11-23 16:50:36 +0900

    오류해결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: large;
+  border-radius: 5px;
+`;
 
 export default function Header() {
   return (
-    <HeadWrapper><Link to={'/register'}>
-      <Button>로그인</Button>  </Link></HeadWrapper>
-  )
+    <HeadWrapper>
+      <Link to={"/register"}>
+        <Button>로그인</Button>{" "}
+      </Link>
+    </HeadWrapper>
+  );
 }
 
 const HeadWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-`
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Detail from "../pages/Detail";
 import Home from "../pages/Home";
 import MyPage from "../pages/MyPage";
