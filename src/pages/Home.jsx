13e1b19d9c52ade838a48e9f6873b791444a1dc9
Dello96 <enadios03@gmail.com>
2023-11-23 15:34:46 +0900

    다크모드, 헤더부분수정, 카드부분 작업중(이미지 불러오기가 안돼서 수정중)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,90 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import { default as styled } from "styled-components";
 import mainImg from "../assets/breadMain.jpg";
+import { GlobalStyles } from "../components/NightMode";
+
+const HomeHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
 const HomeImg = styled.img`
   display: flex;
-  width: 100%;
-  object-fit: cover;
+  width: 200px;
+  height: 80px;
+  margin: 20px;
 `;
 
-const ContentsInput = styled.input`
+const WrappingBtns = styled.div`
   display: flex;
-  margin-left: 100px;
-  width: 300px;
-  height: 100px;
-  justify-content: center;
-  text-align: start;
+`;
+
+const Btns = styled.button`
+  margin: 20px;
+  flex: 0 0 90%;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: transparent;
+  border: 0px;
+`;
+
+const PostBreadBtn = styled.button`
+  background-color: transparent;
+  border: 0px;
+  width: 150px;
+  height: 30px;
+  text-align: center;
+  font-size: 20px;
+`;
+const LoginBtn = styled.button`
+  background-color: transparent;
+  border: 0px;
+  width: 100px;
+  height: 30px;
+  text-align: center;
+  font-size: 20px;
+`;
+const ModeBtn = styled.button`
+  background-color: transparent;
+  border: 0px;
+  width: 100px;
+  height: 30px;
+  text-align: center;
+  font-size: 20px;
 `;
 
 function Home() {
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
     <div>
-      <nav>
-        <HomeImg src={mainImg}></HomeImg>
-      </nav>
+      {/* <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}> */}
+      <GlobalStyles theme={theme} />
       <div>
-        <button>파일첨부</button>
-        <ContentsInput
-          type="text"
-          placeholder="오늘의 빵을 공유해주세요!"
-        ></ContentsInput>
+        <HomeHeader>
+          <HomeImg src={mainImg}></HomeImg>
+          <WrappingBtns>
+            <Btns>
+              <PostBreadBtn>빵 소개하러 가기</PostBreadBtn>
+              <LoginBtn>로그인</LoginBtn>
+              <ModeBtn onClick={themeToggler}>
+                {theme === "light" ? "🌚" : "🌞"}
+              </ModeBtn>
+            </Btns>
+          </WrappingBtns>
+        </HomeHeader>
+        <li>
+          <ul>카드</ul>
+        </li>
       </div>
+      {/* </ThemeProvider> */}
     </div>
   );
 }
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -5,17 +5,17 @@ import MyPage from "../pages/MyPage";
 import Write from "../pages/Write";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/detail" element={<Detail />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/write" element={<Write />} />
-                <Route path="*" element={<Navigate replace to="/" />} />
-            </Routes>
-        </BrowserRouter>
-    );
-    }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default Router;
