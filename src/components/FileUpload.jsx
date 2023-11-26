c4d3ce404113c6a6dc50603e404246915b128c74
Hyeo Min Park <hyeominpark@Hyeos-MacBook-Air.local>
2023-11-27 00:02:28 +0900

    css updated base on areum's figma

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -22,40 +22,52 @@ function FileUpload() {
   };
 
   return (
-    <div>
-      FileUpload
-      <UploadContainer>
-        {/* <img style={{ width: "100px" }} src={thumbnail} alt="썸네일 이미지" /> */}
-        <input type="file" onChange={onImageSelectHandler} />
-        <input type="file" onChange={onImageSelectHandler} />
-        <input type="file" onChange={onImageSelectHandler} />
-        <button onClick={onUploadImageHandler}>Upload</button>
-      </UploadContainer>
-    </div>
+    <UploadContainer>
+      <span>여기에 파일을 업로드 하세요</span>
+      {/* <img style={{ width: "100px" }} src={thumbnail} alt="썸네일 이미지" /> */}
+      <input type="file" onChange={onImageSelectHandler} />
+      <input type="file" onChange={onImageSelectHandler} />
+      <input type="file" onChange={onImageSelectHandler} />
+      <button onClick={onUploadImageHandler}>업로드 하기</button>
+    </UploadContainer>
   );
 }
 
 const UploadContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
-  row-gap: 10px;
+  row-gap: 15px;
 
-  width: 500px;
-  height: 200px;
+  width: 100%;
+  margin: 20px 0;
 
-  border: 1px solid green;
-  padding: 0 30px;
+  border: 1px solid gray;
+  border-radius: 20px;
+  padding: 20px;
+
+  & span {
+    font-size: 20px;
+    font-weight: 700;
+    margin: 10px 0;
+  }
 
   & input {
-    background-color: lightgray;
+    background-color: #eaeaea;
     width: 100%;
     column-gap: 10px;
+    font-size: 14px;
   }
 
   & button {
-    width: 100%;
+    width: 100px;
+    margin: 10px 0 10px auto;
+    padding: 5px 10px;
+
+    font-size: 15px;
+    border: 1px solid gray;
+    border-radius: 10px;
   }
 `;
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,9 +29,61 @@ function Post() {
 
   const post = posts.find((item) => item.id === params.id);
 
+  const formattedDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // getMonth returns 0-11
+    const day = date.getDate();
+
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const ampm = hours > 13 ? "오후 " + (hours - 12) : "오전" + hours;
+
+    return `${year}년 ${month}월 ${day}일 ${ampm}시 ${minutes}분`;
+  };
+
   return (
-    <>
-      <OutContainer>
+    <Container>
+      <PostContainer className="post-container">
+        <ImgContainer className="iamge-section">
+          <img src={images[currentIndex] ?? defaultUser} alt="기본이미지" />
+          <ButtonContainer className="button-container">
+            <button
+              onClick={() => {
+                setCurrentIndex(
+                  currentIndex > 0 ? currentIndex - 1 : images.length - 1,
+                );
+                alert("clicked!");
+              }}
+            >
+              prev
+            </button>
+            <button
+              onClick={() =>
+                setCurrentIndex(
+                  currentIndex < images.length - 1 ? currentIndex + 1 : 0,
+                )
+              }
+            >
+              next
+            </button>
+          </ButtonContainer>
+        </ImgContainer>
+        <TextContainer className="text-section">
+          <h2>{post.postTitle}</h2>
+          <p>{formattedDate(post.createdAt)}</p>
+          <p>소개할 빵: {post.breadType}</p>
+          <p>{post.userName}</p>
+          <Content className="post-content">{post.content}</Content>
+        </TextContainer>
+      </PostContainer>
+
+      <Footer className="footer">
+        <button onClick={() => navigate(`/`)}>홈으로 가기</button>
+        <button onClick={() => onMoveToEditHandler(post.id)}>수정하기</button>
+      </Footer>
+      {/* <OutContainer>
         <PrevBtn
           onClick={() => {
             setCurrentIndex(
@@ -66,24 +118,135 @@ function Post() {
         >
           next
         </NextBtn>
-      </OutContainer>
-    </>
+      </OutContainer> */}
+    </Container>
   );
 }
 
 export default Post;
 
-const Container = styled.li`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
+
+  column-gap: 20px;
+`;
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
-  border: 2px solid black;
-  border-radius: 20px;
-  height: 500px;
-  width: 1200px;
-  margin: 20px;
+  width: 400px;
+  height: 100%;
+
+  position: relative;
+
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    border-radius: 20px;
+  }
 `;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 0 10px;
+
+  position: absolute;
+
+  & button {
+    cursor: pointer;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    padding: 5px;
+  }
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  row-gap: 10px;
+
+  width: 500px;
+  padding: 20px;
+
+  & h2 {
+    font-size: 30px;
+    font-weight: 800px;
+
+    padding: 10px 0;
+  }
+
+  & p {
+    display: flex;
+    flex-direction: column;
+
+    row-gap: 10px;
+
+    font-size: 18px;
+  }
+`;
+
+const Content = styled.div`
+  height: 300px;
+
+  background-color: #e7e6e6;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+
+  & button {
+    font-size: 15px;
+
+    padding: 10px 20px;
+
+    border: 1px solid lightgray;
+    border-radius: 10px;
+  }
+`;
+
+// const Container = styled.li`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   border: 2px solid black;
+//   border-radius: 20px;
+//   height: 500px;
+//   width: 1200px;
+//   margin: 20px;
+// `;
+
+// const NextBtn = styled.button`
+//   cursor: pointer;
+//   /* width: 100px;
+//   height: 50px; */
+//   /* position: absolute; */
+// `;
+
+// const PrevBtn = styled.button`
+//   cursor: pointer;
+//   /* position: absolute; */
+//   /* width: 100px;
+//   height: 50px; */
+// `;
 
 const Review = styled.main`
   flex-direction: row;
@@ -107,16 +270,6 @@ const Slide = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
-const PrevBtn = styled.button`
-  cursor: pointer;
-  width: 100px;
-  height: 50px;
-`;
-const NextBtn = styled.button`
-  cursor: pointer;
-  width: 100px;
-  height: 50px;
 `;
 
 const OutContainer = styled.div`
